a1f9dcca9fd22693e05f5f1bf63b876f84adf9b5
Thomas Lewintre <thomas.lewintre@gendarmerie.interieur.gouv.fr>
2025-05-15 23:08:16 +0200

    Interface enregistrement et login utilisateur

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,13 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(TransportInterface $mailer): Response
     {
+        $email = (new Email())
+            ->to('foo@example.com')
+            ->addTo('bar@example.com')
+            ->cc('cc@example.com')
+            ->addCc('cc2@example.com')
+            ->text('Hello World')
+            ->from('foo@example.com')
+            ->subject('Test email')
+
+            // ...
+        ;
+        $mailer->send($email);
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
