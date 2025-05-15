9cdb4c8b3c2401137d7a5969f45928503cabbb97
Thomas Lewintre <thomas.lewintre@gendarmerie.interieur.gouv.fr>
2025-05-15 22:25:21 +0200

    Installation bootstrap + configuration

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,9 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/global.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+const $ = require('jquery');
+require('bootstrap');
